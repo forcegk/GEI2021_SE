5071ce3baac4b7f24511b2687722cfb80a837a95
Alonso Rodriguez <alonso.rodriguez@udc.es>
2020-10-21 03:05:42 +0200

    Removed unnecesary list

--- p1/FirstARM.s
+++ p1/FirstARM.s
@@ -27,7 +27,6 @@ __Vectors				; Mapped to address 0 at reset
 
 ; - User data section -------------------------------------
 	AREA	UserData, DATA, READWRITE, ALIGN=3
-;LIST	DCD 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60	; Lista de elementos
 ; Unsigned short = 16 bits - 0,31,++
 LIST_v	DCW 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ; Unsigned char = 8 bits - Inicializados a cero
