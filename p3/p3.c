85be3e70065ccfe5fa95d330f1b1a12b1ad93903
Alonso Rodriguez <alonso.rodriguez@udc.es>
2020-11-06 17:09:08 +0100

    remove header

--- p3/p3.c
+++ p3/p3.c
@@ -1,20 +1,4 @@
-/*----------------------------------------------------------------------------
- * Name:    Inline.c
- * Purpose: In-line Assembly Demo
- * Note(s):
- *----------------------------------------------------------------------------
- * This file is part of the uVision/ARM development tools.
- * This software may only be used under the terms of a valid, current,
- * end user licence from KEIL for a compatible version of KEIL software
- * development tools. Nothing else gives you the right to use this software.
- *
- * This software is supplied "AS IS" without warranties of any kind.
- *
- * Copyright (c) 2012 Keil - An ARM Company. All rights reserved.
- *----------------------------------------------------------------------------*/
-
 #include <stdio.h>                /* prototype declarations for I/O functions */
-
 
 extern void SER_Init(void);                                   /* see Serial.c */
 
