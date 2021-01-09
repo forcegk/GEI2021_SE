cb4b63e3976902a3894b7beb73bf2bfaad2822c2
Alonso Rodriguez <alonso.rodriguez@udc.es>
2021-01-09 17:22:57 +0100

    Refactor TT2

--- tt2/doc/make.bat
+++ tt2/doc/make.bat
@@ -3,8 +3,10 @@
 set "FLAGS=--shell-escape"
 
 :: Default
-if "%1"=="" goto :release
-if "%1"=="default" goto :release
+if "%1"=="" goto :refs
+if "%1"=="default" goto :refs
+if "%1"=="refs" goto :refs
+if "%1"=="references" goto :refs
 if "%1"=="release" goto :release
 if "%1"=="dirty" goto :dirty
 if "%1"=="clean" goto :clean
@@ -15,9 +17,16 @@ goto :EOF
 pdflatex %FLAGS% main
 goto :EOF
 
+:refs
+pdflatex %FLAGS% main
+biber main
+pdflatex %FLAGS% main
+goto :EOF
+
 :release
 pdflatex %FLAGS% main
 biber main
+pdflatex %FLAGS% main
 pdflatex %FLAGS% main
 goto :EOF
 
@@ -32,6 +41,7 @@ for %%a in ("*.xml") do del "%%a"
 for %%a in ("*.toc") do del "%%a"
 for %%a in ("*.pyg") do del "%%a"
 for %%a in ("*.out") do del "%%a"
+for %%a in ("preamble\*.aux") do del "%%a"
 for %%a in ("_minted-main\*") do del "%%a"
 rmdir _minted-main
 goto :EOF
